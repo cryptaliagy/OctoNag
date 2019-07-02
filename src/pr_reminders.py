3bf527999fbac77880811c06c5b6bfafcf219408
Natalia Maximo <nmaximo@surveymonkey.com>
2019-07-02 11:44:56 -0400

    moved python files to src/ directory

--- src/pr_reminders.py
+++ src/pr_reminders.py
@@ -67,6 +67,8 @@ def lookup_user(name):
         return user_cache[name]['id']
     else:
         email = name + '@surveymonkey.com'
+    
+    email = 'nmaximo@surveymonkey.com'
 
     params = {
         'email': email
